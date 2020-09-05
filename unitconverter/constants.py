5b9d271320bc4e5de405b248f4aa5b7cf23f7be2
Daniel Ciulinaru <66647414+danielciulinaru@users.noreply.github.com>
2020-09-05 16:24:50 -0400

    unitconverter/constants.py: - added typing

--- unitconverter/constants.py
+++ unitconverter/constants.py
@@ -1,35 +1,27 @@
+from typing import List
+
 FACTOR: float = 1024
-B = "b"
-K = "k"
-KB = "kb"
-M = "m"
-MB = "mb"
-G = "g"
-GB = "gb"
-T = "t"
-TB = "tb"
-P = "p"
-PB = "pb"
-UNITS_B = [B]
-UNITS_KB = [K, KB]
-UNITS_MB = [M, MB]
-UNITS_GB = [G, GB]
-UNITS_TB = [T, TB]
-UNITS_PB = [P, PB]
-UNITS_LIST = UNITS_B + UNITS_KB + UNITS_MB + UNITS_GB + UNITS_TB + UNITS_PB
-STANDARD_UNIT_B = B.upper()
-STANDARD_UNIT_KB = K.upper()
-STANDARD_UNIT_MB = M.upper()
-STANDARD_UNIT_GB = G.upper()
-STANDARD_UNIT_TB = T.upper()
-STANDARD_UNIT_PB = P.upper()
-
-
-class Byte(object):
-    def __repr__(self):
-        return B
-
-
-class KiloByte(object):
-    def __repr__(self):
-        return KB
+B: str = "b"
+K: str = "k"
+KB: str = "kb"
+M: str = "m"
+MB: str = "mb"
+G: str = "g"
+GB: str = "gb"
+T: str = "t"
+TB: str = "tb"
+P: str = "p"
+PB: str = "pb"
+UNITS_B: List = [B]
+UNITS_KB: List = [K, KB]
+UNITS_MB: List = [M, MB]
+UNITS_GB: List = [G, GB]
+UNITS_TB: List = [T, TB]
+UNITS_PB: List = [P, PB]
+UNITS_LIST: List = UNITS_B + UNITS_KB + UNITS_MB + UNITS_GB + UNITS_TB + UNITS_PB
+STANDARD_UNIT_B: str = B.upper()
+STANDARD_UNIT_KB: str = K.upper()
+STANDARD_UNIT_MB: str = M.upper()
+STANDARD_UNIT_GB: str = G.upper()
+STANDARD_UNIT_TB: str = T.upper()
+STANDARD_UNIT_PB: str = P.upper()
